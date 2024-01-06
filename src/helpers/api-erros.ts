5ba388923782839e654b1f37e2ae0afa4478420a
Samuel Conradt Do Amaral <conradtsamuel@gmail.com>
2024-01-06 15:49:07 -0300

    Update api-erros.ts

--- src/helpers/api-erros.ts
+++ src/helpers/api-erros.ts
@@ -6,27 +6,23 @@ export class ApiError extends Error {
   constructor(message: string, statusCode: number, res: Response) {
     super(message);
     this.statusCode = statusCode;
-    res.status(this.statusCode).json({"Error": this.message});
   }
 }
 
 export class BadRequestError extends ApiError {
   constructor(message: string, res: Response) {
     super(message, 400, res);
-    res.status(400).json({"Error": this.message});
   }
 }
 
 export class NotFoundError extends ApiError {
   constructor(message: string, res: Response) {
     super(message, 404, res);
-    res.status(404).json({"Error":this.message});
   }
 }
 
 export class UnauthorizedError extends ApiError {
   constructor(message: string, res: Response) {
     super(message, 401, res);
-    res.status(401).json({"Error": this.message});
   }
 }
