c5c76c8e5aab82fecbf2f8ac3727068a4fa13a0e
Samuel Conradt Do Amaral <conradtsamuel@gmail.com>
2024-02-02 14:11:15 -0300

    Cors Teste 2

--- src/server.ts
+++ src/server.ts
@@ -23,7 +23,6 @@ app.use(helmet());
 
 
 const corsOptions = {
-  origin: *,
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   credentials: true,
   exposedHeaders: "Authorization",
