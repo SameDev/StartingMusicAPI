dc5f1ccba3f9482afa6f191e31fa98608472285e
SameDev <conradtsamuel@gmail.com>
2024-02-05 15:54:15 -0300

    Payload Change Limit

--- src/server.ts
+++ src/server.ts
@@ -16,8 +16,8 @@ import playbackRouters from "./routes/playbackRouters";
 
 const app = express();
 
-app.use(express.json());
-app.use(bodyParser.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 
