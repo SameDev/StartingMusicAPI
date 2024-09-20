4da8f056ccfc7cb646b25f0c5810e2c30cedf99f
SameDev <conradtsamuel@gmail.com>
2024-09-19 21:27:31 -0300

    Correções no delete e update

--- src/routes/albumRouters.ts
+++ src/routes/albumRouters.ts
@@ -9,8 +9,8 @@ router.get("/:id", AlbumController.listAll);
 router.post("/:id/add-song", AlbumController.addAlbumSong)
 
 router.post("/create", AlbumController.create);
-router.delete("/delete", AlbumController.delete);
-router.post("/update", AlbumController.update)
+router.delete("/delete/:id", AlbumController.delete);
+router.post("/update/:id", AlbumController.update)
 
 export default router;
 
